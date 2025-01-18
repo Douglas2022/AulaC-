71af6b2bcc4c9595c8c6bdf92d37b3008b571d29
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-18 12:31:56 -0300

    Mudanca na tela

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using tabuleiro;
-namespace Xadrez.tabuleiro
+
+namespace Xadrez
 {
     internal class Tela
     {
-        public static void ImprimirTabuleiro( Tabuleiro Tab)
+        public static void ImprimirTAbuleiro(Tabuleiro Tab)
         {
-            for (int i = 0; i<Tab.Linhas; i++)
+            for (int i = 0; i < Tab.Linhas; i++)
             {
                 for (int j = 0; j < Tab.Colunas; j++)
                 {
-                    if(Tab.peca(i,j) == null)
+                    if (Tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
-                        
                     }
                     else
                     {
                         Console.Write(Tab.peca(i, j) + " ");
                     }
-                                     
+
                 }
                 Console.WriteLine();
             }
