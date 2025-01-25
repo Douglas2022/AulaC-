0e1826873bbc162cdb8ac94883daf6cb55ee0f54
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-25 12:08:41 -0300

    Direção do Rei (Norte)

--- Xadrez/xadrez/Rei.cs
+++ Xadrez/xadrez/Rei.cs
@@ -11,5 +11,23 @@ namespace xadrez
         {
             return "R";
         }
+        private bool PodeMover(Posicaocs Pos)
+        {
+            Peca P = Tab.peca(Pos);
+            return P == null || P.Cor != Cor;
+        }
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicaocs Pos = new Posicaocs(0,0);
+
+            // acima
+            Pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if (Tab.PosicaoValida(Pos) && PodeMover(Pos))
+            {
+                mat[Pos.Linha, Pos.Coluna] = true;
+            } 
+        }
     }
 }
