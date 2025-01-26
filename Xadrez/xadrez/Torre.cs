8cdee88d13b2da3e78088035e0654615d04149e1
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-25 21:19:44 -0300

    Adicionando o movimento acima da Torre

--- Xadrez/xadrez/Torre.cs
+++ Xadrez/xadrez/Torre.cs
@@ -11,5 +11,32 @@ namespace xadrez
         {
             return "T";
         }
+        private bool PodeMover(Posicaocs Pos)
+        {
+            Peca P = Tab.peca(Pos);
+            return P == null || P.Cor != Cor;
+                     
+        }
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicaocs Pos = new Posicaocs(0, 0);
+
+            //Acima
+            Pos.DefinirValores(Posicao.Linha -1, Posicao.Coluna);
+            while(Tab.PosicaoValida(Pos) && PodeMover(Pos))
+            {
+                mat[Pos.Linha,Pos.Coluna] = true;
+                if(Tab.peca(Pos) == null && Tab.peca(Pos).Cor != Cor) 
+                {
+                    break;
+                }
+                Pos.Linha = Pos.Linha -1;
+            }
+
+
+            return mat;
+        }
     }
 }
