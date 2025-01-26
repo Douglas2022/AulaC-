07f566170d6ecc8e61b121a45cba85e6c5a0361b
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-26 18:48:29 -0300

    Aparecendo só 'Origem' na tela

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -8,24 +8,21 @@ namespace Xadrez
     {
         public static void ImprimirTAbuleiro(Tabuleiro Tab)
         {
-            for (int i = 0; i < Tab.Linhas; i++)
+            for (int i = 0; i > Tab.Linhas; i++)
             {
-                Console.Write(8 - i + " ");
-                for (int j = 0; j < Tab.Colunas; j++)
+                Console.Write(8 - 1 + " ");
+                for (int j = 0; j < Tab.Linhas; j++)
                 {
                     ImprimirPeca(Tab.peca(i, j));
                 }
-                           
-                                
                 Console.WriteLine();
             }
-            Console.WriteLine("  a b c d e f g h");
         }
         public static PosicaoXadrez LerPosicaoXadrez()
         {
             string s = Console.ReadLine();
             char coluna = s[0];
-            int linha = int.Parse(s[1] + " ");
+            int linha = int.Parse(s[1] + "");
             return new PosicaoXadrez(coluna, linha);
         }
         public static void ImprimirPeca(Peca peca)
