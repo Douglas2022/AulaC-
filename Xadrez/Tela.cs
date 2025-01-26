1796fa411ebb1d89ad352a32c66923dda6668f73
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-26 19:11:08 -0300

    Está desalinhado só os traços do xadrez

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -8,43 +8,44 @@ namespace Xadrez
     {
         public static void ImprimirTAbuleiro(Tabuleiro Tab)
         {
-            for (int i = 0; i > Tab.Linhas; i++)
+            for (int i = 0; i < Tab.Linhas; i++)
             {
-                Console.Write(8 - 1 + " ");
-                for (int j = 0; j < Tab.Linhas; j++)
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < Tab.Colunas; j++)
                 {
                     ImprimirPeca(Tab.peca(i, j));
                 }
-                Console.WriteLine();
+                Console.WriteLine("-");
             }
+            Console.WriteLine("  a b c d e f g h");
         }
         public static PosicaoXadrez LerPosicaoXadrez()
         {
             string s = Console.ReadLine();
             char coluna = s[0];
-            int linha = int.Parse(s[1] + "");
+            int linha = int.Parse(s[1].ToString());
             return new PosicaoXadrez(coluna, linha);
         }
         public static void ImprimirPeca(Peca peca)
         {
             if (peca == null)
             {
-                Console.Write("- ");
+                Console.Write("-");
             }
             else
             {
                 if (peca.Cor == Cor.Branca)
                 {
-                    Console.Write(peca);
+                    Console.Write(peca + " ");
                 }
                 else
                 {
                     ConsoleColor aux = Console.ForegroundColor;
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(peca);
+                    Console.Write(peca + " ");
                     Console.ForegroundColor = aux;
                 }
-                Console.WriteLine(" ");
+                
             }
 
         }
